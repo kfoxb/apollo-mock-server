52a401c15caaf3fb3e5dc38fc2b770f6b5274c2b
Kyle Bradford <kfoxb@kfoxb.com>
2019-05-31 13:55:59 -0700

    feat: mock local schema and forward all others to remote

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 const fs = require("fs");
 const path = require("path");
-const { ApolloServer, gql } = require('apollo-server');
+const { makeRemoteExecutableSchema, ApolloServer, gql, introspectSchema } = require('apollo-server');
+const fetch = require('node-fetch')
+const { print } = require('graphql');
+const { addMockFunctionsToSchema, makeExecutableSchema, mergeSchemas } = require('graphql-tools')
 
-const schemaPath = path.join(__dirname, "schema.graphql")
+const fetcher = async ({ query: queryDocument, variables, operationName, context }) => {
+  const query = print(queryDocument);
+  const fetchResult = await fetch('http://localhost:8075/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({ query, variables, operationName })
+  });
+  return fetchResult.json();
+};
 
-if (!fs.existsSync(schemaPath)) {
-  throw new Error("Schema doesn't exist")
+const createSchema = async () => {
+  const schema = makeRemoteExecutableSchema({
+    schema: await introspectSchema(fetcher),
+    fetcher,
+  });
+  return schema
 }
 
-const schema = fs.readFileSync(schemaPath).toString();
+const start = async () => {
+  const schemaPath = path.join(__dirname, "schema.graphql")
+  if (!fs.existsSync(schemaPath)) {
+    throw new Error("Schema doesn't exist")
+  }
+  const mockedSchema = makeExecutableSchema({ typeDefs: fs.readFileSync(schemaPath).toString() });
+  addMockFunctionsToSchema({ schema: mockedSchema });
+  const gatewaySchema = await createSchema()
+  const schema = mergeSchemas({
+    schemas: [
+      gatewaySchema,
+      mockedSchema,
+    ],
+  });
+  const server = new ApolloServer({
+    schema,
+  });
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+  });
+}
 
-const server = new ApolloServer({
-  typeDefs: gql(schema),
-  mocks: true,
-});
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
-});
+start()
